5e082d0111e507b8c47bd05ad67bafcdfd6776ed
Nagesh <nageshtirumani6@gmail.com>
2025-03-03 14:10:22 +0530

    added spring boot init commit.

---
+++ src/main/java/com/cloud/ms/training/BootTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.ms.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/ms/training/BootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.ms.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
